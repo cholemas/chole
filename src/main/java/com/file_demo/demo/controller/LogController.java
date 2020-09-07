54d20e50a21105d67f8748268f2759cfc1fe4921
rookie <420471905@qq.com>
2020-09-07 14:14:43 +0800

    add controller

---
+++ src/main/java/com/file_demo/demo/controller/LogController.java
@@ -0,0 +1,16 @@
+package com.file_demo.demo.controller;
+
+import com.file_demo.demo.rout.Result;
+
+/**
+ * @author baichengwei
+ * @Description
+ * @date 2020/9/7 14:10
+ */
+public class LogController {
+
+    public Result search(){
+        return Result.success("查询成功",null);
+    }
+
+}
